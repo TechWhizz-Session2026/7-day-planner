c50ac0a1abeb8cd5f99edd536b690c93d5274af2
Oluwatobiloba Oluwadare <tobidechamp15@gmail.com>
2026-09-03 17:47:54 +0100

    implmented the sidebar functiona;ity

---
+++ src/Dashboard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+
+const Dashboard = () => {
+  return (
+    <>
+      <div>
+        Nabar comming in here
+      </div>
+      <div className="dashboard">
+        <section className="dashboard-nav">
+          <NavLink to="/dashboard/home">Plans Home</NavLink>
+          <NavLink to="/dashboard/new-plans">New Plans</NavLink>
+        </section>
+        <Outlet />
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ src/PlansHome.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlansHome = () => {
+  return <div>PlansHome</div>;
+};
+
+export default PlansHome;
